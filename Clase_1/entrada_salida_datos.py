6239c167aa7ed54b12b34e8a3ced99ea36ffb6fa
Candela <candemartt@gmail.com>
2022-09-02 18:28:35 -0300

    Entradas y salidas de datos

---
+++ Clase_1/entrada_salida_datos.py
@@ -0,0 +1,31 @@
+# Salida de datos
+print("Hola Mundo!... ")
+print("Desde ' visual")
+
+# Otras funciones del print
+# Operacion matematica
+print(2+2)
+
+# Salto de linea
+print("Esto es un salto \n de linea")
+
+# Esto es una tabulacion
+print("Con estos se \t tabula")
+
+# Concatenar
+print("Hola "  + "Mundo")
+print("Hola" + " Mundo")
+print("Hola" + " " + "Mundo")
+
+# Sep
+print("Hola","Mundo\n", sep= '+')
+
+# End
+print("Hola Mundo!... ", end= '\t')
+print("Desde ' visual")
+
+# Entrada de datos
+print("Ingrese un mesaje: ")
+input()
+input("Ingrese un mensaje: ")
+
