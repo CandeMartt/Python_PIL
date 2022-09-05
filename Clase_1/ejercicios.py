accba1a63b97359442054272f89231bf20e6a6ee
Candela <candemartt@gmail.com>
2022-09-04 21:19:10 -0300

    Tarea

---
+++ Clase_1/ejercicios.py
@@ -0,0 +1,17 @@
+# Ejercicio 1
+print("a \t b \t c") 
+print("2.4 \t -3.21 \t 47.8") 
+
+# Ejercicio 2
+
+# Ejercicio 3 
+palabra = input("Ingrese una palabra: ")
+longitud =((len(palabra)) * '*')
+print(longitud)
+
+# Ejercicio 4 - Diccionario
+d1 = {
+    palabra :  longitud
+} 
+
+print(d1)
