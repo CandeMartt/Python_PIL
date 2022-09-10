eeb1b9c26daca6b9158d643cfe0555490b8bc1c5
Candela <candemartt@gmail.com>
2022-09-09 21:23:02 -0300

    Clase del 09/09/2022

--- Clase_4/funciones.py
+++ Clase_4/funciones.py
@@ -13,3 +13,28 @@ def sumar(a, b):
 resultado_suma = sumar(2, 3)
 print(resultado_suma)
 print(sumar(2,3))
+
+# Funcion 2
+
+def resta(): # Indico que no va a recibir parametros
+    resultado = 2 - 3 # Esta cuenta es permanenete
+    return resultado
+
+print(resta())
+
+# Funcion 3
+
+def saludo(nombre):
+    print('Hola', nombre) # De esta forma imprime el resultado sin pasarselo a return
+
+nombre = input("Ingrese su nombre: ")
+saludo(nombre)
+
+# Funcion 4
+def saludo(nombre, cantidad_saludos):
+    lista_nombre = [] # Como la lista esta dentro de la función es local, es decir le pertenece solo a la función. Si estubiera por fuera seria local
+    for i in range(cantidad_saludos):
+        nombre = input("Ingrese su nombre: ")
+        print('Hola', nombre) # De esta forma imprime el resultado sin pasarselo a return
+        lista_nombre.append(nombre) # Guarda los nombres en la lista
+
