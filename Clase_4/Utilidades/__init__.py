08b062f6a7a364e2b96bd4ceed1ba2214169f155
Candela <candemartt@gmail.com>
2022-09-11 21:44:40 -0300

    Paquetes

---
+++ Clase_4/Utilidades/__init__.py
@@ -0,0 +1,2 @@
+# El init le indica a Python que esta carpeta tiene que ser empezada a trackearse como si fuese un paquete
+# Todo lo que ponga aca Python automaticamente la define como paquete
--- Clase_4/paquetes.py
+++ Clase_4/paquetes.py
@@ -1,6 +1,8 @@
 # import pandas as pd # Importo todo Pandas. Con as le asignamos un pseudonimo
 # pd.DataFrame() # Trabajar con la función DataFrame
 
-from pandas import DataFrame as df # Solo puedo utilizar esta función. De esta forma evito importas todo Pandas
+# from pandas import DataFrame as df # Solo puedo utilizar esta función. De esta forma evito importas todo Pandas
+#data = df()
 
-data = df()
+from funciones import saludo, orden
+saludo()
