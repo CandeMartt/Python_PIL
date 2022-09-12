bee75ad2b3c10c13557ecfa240a5f8baaed7adc2
Candela <candemartt@gmail.com>
2022-09-11 21:11:45 -0300

    Contenido de la clase 3 y generandoel requirements

---
+++ Clase_4/paquetes.py
@@ -0,0 +1,6 @@
+# import pandas as pd # Importo todo Pandas. Con as le asignamos un pseudonimo
+# pd.DataFrame() # Trabajar con la función DataFrame
+
+from pandas import DataFrame as df # Solo puedo utilizar esta función. De esta forma evito importas todo Pandas
+
+data = df()
